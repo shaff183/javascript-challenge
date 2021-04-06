796b8e1f52809d20d676d24fc0164ae9c66b1db7
Eric Shaffer <EricShaffer@chriss-mbp-3.lan>
2021-04-06 13:42:27 -0500

    getting all of the data to display on website

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -4,4 +4,23 @@ console.log("app.js has loaded");
 var tableData = data;
 
 // YOUR CODE HERE!
-console.log(tableData);
+
+// get a refereence to the tbody section where the table data will go
+var tbody = d3.select("tbody");
+
+// for each array of data, insert into the tbody section
+tableData.forEach(ufoSiting => {
+    // add a new row for each entry
+    var newRow = tbody.append("tr");
+
+    // loop through each entry 
+    Object.entries(ufoSiting).forEach(function([key, value]) {
+        // testing if we are logging the correct values for each entry
+        console.log(key, value);
+
+        // add these to the table, each with a new cell
+        var newCell = newRow.append("td");
+        newCell.text(value);
+    })
+
+})
