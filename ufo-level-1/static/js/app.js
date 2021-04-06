e56414fde18383bef42fedc6625482b34d9b21d7
Eric Shaffer <EricShaffer@chriss-mbp-3.lan>
2021-04-06 14:23:20 -0500

    able to get filtered data to append to end of table, need to work on clearing table before though

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -24,3 +24,51 @@ tableData.forEach(ufoSiting => {
     })
 
 })
+
+// create event handlers to run when user inputs a date
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+// create the event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// create funtion to filter through data matching criteria
+function runEnter(){
+
+    // prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    // filter through the data based on the date provided by user
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+    console.log(filteredData);
+
+    // clear the table of its data before adding filtered data
+    
+
+    // display only the data that matches
+    filteredData.forEach(ufoSiting => {
+        // add a new row for each entry
+        var newRow = tbody.append("tr");
+    
+        // loop through each entry 
+        Object.entries(ufoSiting).forEach(function([key, value]) {
+            // testing if we are logging the correct values for each entry
+            console.log(key, value);
+    
+            // add these to the table, each with a new cell
+            var newCell = newRow.append("td");
+            newCell.text(value);
+        })
+    
+    })
+};
+
